82f1b12e4c23ad56da349f8e8ecfd15ac891dce9
MorrisMorrison <mwlltrdev@gmail.com>
2019-12-22 20:56:42 +0100

    add support for passing c# functions as callback functions to js methods invoked by js runtime

--- HiddenSolutionsClient/wwwroot/jsinterop.js
+++ HiddenSolutionsClient/wwwroot/jsinterop.js
@@ -10,10 +10,22 @@ materializeHelpers = {
         });  
     },
     
-    initChips:function () {
+    initChips:function (callback) {
         $('.chips-placeholder').chips({
             placeholder: 'Enter a tag',
             secondaryPlaceholder: '+Tag',
+            onChipAdd: (event, chip) => {
+                callback(event[0].M_Chips.chipsData);
+            },
         });
     }
+};
+
+window.callbackProxy =  function(dotNetInstance, callMethod, callbackMethod){
+    // Execute function that will do the actual job
+    materializeHelpers[callMethod](function(result){
+        // Invoke the C# callback method passing the result as parameter
+        return dotNetInstance.invokeMethodAsync(callbackMethod, result);
+    });
+    return true;
 };
