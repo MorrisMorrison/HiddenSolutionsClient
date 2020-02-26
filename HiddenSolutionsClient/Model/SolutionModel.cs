c179367ca10eea431071dc4a8fa6a4a4dc08d464
MorrisMorrison <mwlltrdev@gmail.com>
2020-02-26 23:28:26 +0100

    - add simple category and tag filter to search result view - instantly validate create solution form

--- HiddenSolutionsClient/Model/SolutionModel.cs
+++ HiddenSolutionsClient/Model/SolutionModel.cs
@@ -1,39 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Mime;
+using System.ComponentModel.DataAnnotations;
 
 namespace HiddenSolutionsClient.Model
 {
     public class SolutionModel
     {
         public string Id { get; set; }
-        public string Title { get; set; }
-        public string ProblemDescription { get; set; }
-        public string SolutionDescription { get; set; }
-        public Category Category { get; set; } = new Category();
-        public IList<Tag> Tags { get; set; } = new List<Tag>();
+        [Required] public string Title { get; set; }
+        [Required] public string ProblemDescription { get; set; }
+        [Required] public string SolutionDescription { get; set; }
+        [Required] public Category Category { get; set; } = new Category();
+        [Required] public IList<Tag> Tags { get; set; } = new List<Tag>();
         public IList<Image> Images { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        [Required]
         public string Link { get; set; }
         public DateTime LinkLastVisited { get; set; } = DateTime.Today.Date;
+    }
+
+    public class Image
+    {
+        public Path Path { get; set; }
     }
 
     public class Category
     {
         public string Name { get; set; }
     }
-
+    
     public class Tag
     {
         public string Value { get; set; }
     }
-
-    public class Image
-    {
-            public Path Path { get; set; }
-    }
     
+
     public class Path
     {
         public string Value { get; set; }
--- HiddenSolutionsClient/wwwroot/jsinterop.js
+++ HiddenSolutionsClient/wwwroot/jsinterop.js
@@ -29,3 +29,14 @@ window.callbackProxy =  function(dotNetInstance, callMethod, callbackMethod){
     });
     return true;
 };
+
+window.getSelectedValues = function(sel) {
+    var results = [];
+    var i;
+    for (i = 0; i < sel.options.length; i++) {
+        if (sel.options[i].selected) {
+            results[results.length] = sel.options[i].value;
+        }
+    }
+    return results;
+};
